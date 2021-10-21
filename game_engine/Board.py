136d27e66cd0ea9a76822b7d6731c14d8c0e7a2b
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-10-21 17:12:34 +0200

    BoardState class

--- game_engine/Board.py
+++ game_engine/Board.py
@@ -1,10 +1,23 @@
 import numpy as np
 
 
+class BoardState(np.ndarray):
+    """ Class used for representation of current BoardState"""
+
+    def __new__(cls, pawns, fields, player):
+        """"""
+        pawns = np.asarray(pawns, dtype=int)
+        fields = np.asarray(fields, dtype=int)
+        player = np.asarray(player, dtype=int)
+        if not player.shape == fields.shape == pawns.shape == (9, 7):
+            raise ValueError("Wrong shape of input board shapes.")
+        obj = np.stack((pawns, fields, player), axis=0).view(cls)
+        return obj
+
+
 def initialize_board():
-    board_state = np.zeros((3, 9, 7), dtype=int)
-    board_state[2, :, :] = 1
-    board_state[1, :, :] = np.array([
+    player = np.ones((9, 7), dtype=int)
+    fields = np.array([
         [0, 0, 2, 0, 2, 0, 0],
         [0, 0, 0, 2, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
@@ -15,7 +28,7 @@ def initialize_board():
         [0, 0, 0, 2, 0, 0, 0],
         [0, 0, 2, 0, 2, 0, 0],
     ])
-    board_state[0, :, :] = np.array([
+    pawns = np.array([
         [-7, 00, 00, -9, 00, 00, -6],
         [00, -3, 00, 00, 00, -2, 00],
         [-1, 00, -5, 00, -4, 00, -8],
@@ -26,4 +39,9 @@ def initialize_board():
         [00, +2, 00, 00, 00, +3, 00],
         [+6, 00, 00, +9, 00, 00, +7],
     ])
+    board_state = BoardState(pawns, fields, player)
     return board_state
+
+
+a = initialize_board()
+tmp = 1
