a7d520cf0c8a9cea377ab3bc34260ce1d84b801a
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-01 17:41:38 +0200

    Added dummy model select component

--- src/frontend/src/components/ModelSelect/ModelSelect.jsx
+++ src/frontend/src/components/ModelSelect/ModelSelect.jsx
@@ -5,8 +5,8 @@ function ModelSelect() {
     const [models, setModels] = useState(["first_model", "model_2", "model_3", "model_4"])
     const [versions, setVersions] = useState([1, 2, 4, 5, 6])
 
-    const modelOptions = models.map((model) => <option value={model}>{model}</option>)
-    const modelVersions = versions.map((version) => <option value={version}>V{version}.0</option>)
+    const modelOptions = models.map((model) => <option value={model} key={model}>{model}</option>)
+    const modelVersions = versions.map((version) => <option value={version} key={version}>V{version}.0</option>)
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -14,12 +14,12 @@ function ModelSelect() {
 
     return (
         <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="ModelNameSelect">Model Name</label>
-            <select name="Models" id="ModelNameSelect">
+            <label htmlFor="model_name">Model Name</label>
+            <select name="Models" id="model_name">
                 {modelOptions}
             </select>
-            <label htmlFor="ModelVersionSelect">Model Version</label>
-            <select name="Versions" id="ModelVersionSelect">
+            <label htmlFor="model_version">Model Version</label>
+            <select name="Versions" id="model_version">
                 {modelVersions}
             </select>
             <button type="submit">Switch Model</button>
