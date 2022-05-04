6339942d4157ce75bf9dc50c6ddf71eb687529a9
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-04 15:45:36 +0200

    more training

--- src/backend/game/__init__.py
+++ src/backend/game/__init__.py
@@ -6,7 +6,7 @@ from .unit import Unit, Units, EMPTY, Den
 from . import moves
 from .moves import Move, UnitMove
 
-from .models import AbstractModel, value_policy_model
+from .models import AbstractModel, value_policy_model, ValuePolicyModel
 
 from .cell import Cell, GroundStates
 from .board import Board, Position, BoardTensor
