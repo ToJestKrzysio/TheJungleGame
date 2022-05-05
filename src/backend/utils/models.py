91e07c1f57f9eece1634657e96e1231618739ae3
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-05 21:58:14 +0200

    removed unused files

--- src/backend/utils/models.py
+++
@@ -1,7 +0,0 @@
-import os
-from typing import Tuple
-
-
-def get_model_versions(model_name: str) -> Tuple[int, ...]:
-    versions = os.listdir(f"./data/models/{model_name}")
-    return tuple(sorted((int(version) for version in versions), reverse=True))
